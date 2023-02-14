2b8b495266c325a1a5669f79559496d2b9ea2710
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2023-02-14 13:41:49 -0800

    server input to connect to MongoDB

--- game-backlog/src/components/Login/login.js
+++ game-backlog/src/components/Login/login.js
@@ -15,6 +15,7 @@ import {
   MDBInput,
   MDBBtn,
 } from "mdb-react-ui-kit";
+import { checkPassword, validateEmail } from "../../utils/helpers";
 
 function Login() {
   const [fillActive, setFillActive] = useState("tab-login");
@@ -25,6 +26,53 @@ function Login() {
     }
 
     setFillActive(value);
+  };
+const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else {
+      setPassword(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or username is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    if (!checkPassword(password)) {
+      setErrorMessage(
+        `Choose a more secure password for the account: ${userName}`
+      );
+      return;
+    }
+    //  add mutation to add a user make back end in mern 26 copy on config ans user.js and thought in schema use 41 33 34 get rid of comment 
+    //  use the resolvers use the auth and server.js make sure to nock out backend copy server.js 
+    alert(`Hello ${userName}`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setPassword('');
+    setEmail('');
   };
 
   return (
@@ -102,16 +150,22 @@ function Login() {
 
                       <MDBInput
                         className="mb-4"
+                        name = "userName"
+                        value = {userName}
                         type="email"
                         id="loginName"
                         label="Email or username"
+                        onchange = {handleInputChange}
                       />
 
                       <MDBInput
                         className="mb-4"
+                        name = "password"
+                        value = {password}
                         type="password"
                         id="loginPassword"
                         label="Password"
+                        onchange = {handleInputChange}
                       />
 
                       <MDBRow className="mb-4">
